e8f7414da0261d599023ec9c2d352ae571d6189c
Kyle Hornberg <kyle.hornberg@gmail.com>
2016-03-23 07:58:31 -0500

    Add correct dependencies

--- setup.py
+++ setup.py
@@ -8,7 +8,8 @@ setup(
     install_requires=[
         'click',
         'PyGithub',
-        'excutor'
+        'executor',
+        'jenkinsapi',
     ],
     entry_points='''
         [console_scripts]
