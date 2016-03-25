0be0063adec4cf696217f9168bcec890f3cc9a7a
Kyle Hornberg <kyle.hornberg@gmail.com>
2016-03-25 05:24:52 -0500

    Version: 0.2.0 -> 0.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='umbrella',
-    version='0.2.0',
+    version='0.3.0',
     py_modules=['um'],
     include_package_data=True,
     install_requires=[
